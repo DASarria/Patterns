8868fd481ee36efd1442420fbdaf28d8d434b602
USUARIO <santiago.amador-d@mail.escuelaing.edu.co>
2025-01-29 10:39:46 -0500

    Terminado el Esqueleto de Patterns y subiendo imagenes

--- src/main/java/edu/eci/cvds/patterns/Shapes/QuadrilateralFactory.java
+++ src/main/java/edu/eci/cvds/patterns/Shapes/QuadrilateralFactory.java
@@ -3,9 +3,8 @@ package edu.eci.cvds.patterns.shapes;
 import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
 
 public class QuadrilateralFactory implements ShapeFactory{
-
     @Override
-    public Shape create(RegularShapeType type) {
+    public Shape create() {
         return new Quadrilateral();
     }
 
