07e9635bd6fa22e989069dfddc66c1c998f0897f
David Sarria <davidsarria911@hotmail.com>
2025-02-02 10:33:30 -0500

    Esqueleto completo

--- src/main/java/edu/eci/cvds/patterns/Shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/Shapes/ShapeFactory.java
@@ -1,8 +1,5 @@
 package edu.eci.cvds.patterns.shapes;
-import edu.eci.cvds.patterns.shapes.concrete.Hexagon;
-import edu.eci.cvds.patterns.shapes.concrete.Pentagon;
-import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
-import edu.eci.cvds.patterns.shapes.concrete.Triangle;
+import edu.eci.cvds.patterns.shapes.concrete.*;
 
 public class ShapeFactory {
     public static Shape create(RegularShapeType type) {
