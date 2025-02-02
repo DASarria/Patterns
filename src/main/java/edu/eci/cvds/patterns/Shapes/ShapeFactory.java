bdfca5d34527372e3ed3edbc8f4fbbfb3454eed0
David Sarria <davidsarria911@hotmail.com>
2025-02-01 21:56:00 -0500

    esqueleto

--- src/main/java/edu/eci/cvds/patterns/Shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/Shapes/ShapeFactory.java
@@ -1,5 +1,21 @@
 package edu.eci.cvds.patterns.shapes;
+import edu.eci.cvds.patterns.shapes.concrete.Hexagon;
+import edu.eci.cvds.patterns.shapes.concrete.Pentagon;
+import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
+import edu.eci.cvds.patterns.shapes.concrete.Triangle;
 
-public interface ShapeFactory {
-    public Shape create();
+public class ShapeFactory {
+    public static Shape create(RegularShapeType type) {
+        switch (type){
+            case Triangle:
+                return new Triangle();
+            case Quadrilateral:
+                return new Quadrilateral();
+            case Pentagon:
+                return new Pentagon();
+            case Hexagon:
+                return new Hexagon();
+        }
+        return null;
+    }
 }
--- src/main/java/edu/eci/cvds/patterns/Shapes/ShapeMain.java
+++ src/main/java/edu/eci/cvds/patterns/Shapes/ShapeMain.java
@@ -2,40 +2,26 @@ package edu.eci.cvds.patterns.shapes;
 
 public class ShapeMain {
 
-    public static void main(String[] args) {
-        if (args == null || args.length != 1) {
-            System.err.println("Parameter of type RegularShapeType is required.");
-            return;
-        }
-        try {
-            RegularShapeType type = RegularShapeType.valueOf(args[0]);
-            Shape shape = null;
-            switch (type){
-                case Triangle:
-                    shape = new TriangleFactory().create();
-                    break;
-                case Quadrilateral:
-                    shape = new QuadrilateralFactory().create();
-                    break;
-                case Pentagon:
-                    shape = new PentagonFactory().create();
-                    break;
-                case Hexagon:
-                    shape = new HexagonFactory().create();
-                    break;
-            }
-            System.out.println(
-                    String.format(
-                            "Successfully created a %s with %s sides.",
-                            type,
-                            shape.getNumberOfEdges()
-                    )
-            );
-        } catch (IllegalArgumentException ex) {
-            System.err.println(
-                    "Parameter '" + args[0] + "' is not a valid RegularShapeType"
-            );
-            return;
-        }
+  public static void main(String[] args) {
+    if (args == null || args.length != 1) {
+      System.err.println("Parameter of type RegularShapeType is required.");
+      return;
     }
+    try {
+      RegularShapeType type = RegularShapeType.valueOf(args[0]);
+      Shape shape = ShapeFactory.create(type);
+      System.out.println(
+        String.format(
+          "Successfully created a %s with %s sides.",
+          type,
+          shape.getNumberOfEdges()
+        )
+      );
+    } catch (IllegalArgumentException ex) {
+      System.err.println(
+        "Parameter '" + args[0] + "' is not a valid RegularShapeType"
+      );
+      return;
+    }
+  }
 }
